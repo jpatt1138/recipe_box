8a651ce57b08a0e5b885d77c205ebc1a51f74b33
Courtney Phillips & Jim Patt <student@epicodus.com>
2015-05-14 15:43:39 -0700

    Tweak styles.

--- app.rb
+++ app.rb
@@ -18,36 +18,20 @@ patch('/recipe/:id') do
   id = params.fetch("id")
   @recipe = Recipe.find(params.fetch("id").to_i())
 
-
-  # category_ids = []
-  # categories_array.each do |category|
-  #   category_id = category.id()
-  #   category_ids.push(category_id)
-  # end
-  # found_categories =
-  # category_ids.each do |id|
-
   if params[:category]
     index = 0
     new_categories = params.fetch("category")
     old_categories = @recipe.categories()
-
     old_categories.each() do |category|
       category.destroy()
     end
-
     new_categories.each do |category|
       new_category = Category.create({:name => category})
-
       new_category.recipes.push(@recipe)
-      binding.pry
-
     end
   else
     @recipe.categories = @recipe.categories
-
   end
-
 
   if params[:ingredients]
     ingredients = params.fetch("ingredients")
@@ -67,11 +51,6 @@ patch('/recipe/:id') do
 
   end
   @recipes = Recipe.all()
-  # ingredients_array = ingredients.split("; ")
-  # ingredients_array.each do |ingredient|
-  #   ingredient_new = Ingredient.create({ :name => ingredient})
-  #   ingredient_new.recipes.push(new_recipe)
-
   redirect to("/recipe/#{@recipe.id}")
 end
 
@@ -112,7 +91,6 @@ get('/category/:id') do
   @recipes = @category.recipes()
   erb(:category)
 end
-
 
 get('/recipe/:id/edit') do
   id = params.fetch('id')
